22edc298c505bb10b94bece9dc38597130d70a1a
ap2144839 <ap2144839@gmail.com>
2018-05-01 10:24:27 +0300

    Pro.java changing

--- Pro.java
+++ Pro.java
@@ -1,13 +1,13 @@
 public class Pro {
-public static void main (String args[]){
-   System.out.println("What's up!");
-   byte b;
-   short s;
-   int i = 10;
-   long l;
-   float f;
-   double d;
-   double q = ((double)i) / 3;
-   System.out.println(q);
-}
+   public static void main (String args[]){
+      System.out.println("What's up!");
+      byte b;
+      short s;
+      int i = 10;
+      long l;
+      float f;
+      double d;
+      double q = ((double)i) / 3;
+      System.out.println(q);
+   }
 }
